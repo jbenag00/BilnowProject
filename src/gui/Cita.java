33a04f28363653da1dbc9245ddee63894173990f
Javier <Javier@DESKTOP-BP99DUU>
2018-01-07 23:57:37 +0100

    un par de cambios

--- src/gui/Cita.java
+++ src/gui/Cita.java
@@ -1,11 +1,14 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import com.toedter.calendar.JCalendar;
 
 import control.Mascota;
 
@@ -22,7 +25,18 @@ public class Cita {
 	private JFrame frame;
 
 	private static Mascota mascota_Cita=null;
-	private JTable tabla_Horarios;
+	
+	private final String[] horarios= {"8:00",
+									  "9:00",
+									  "10:00",
+									  "11:00",
+									  "12:00",
+									  "13:00",
+									  "16:00",
+									  "17:00",
+									  "18:00",
+									  "19:00",
+									  "20:00"};
 
 	/**
 	 * Launch the application.
@@ -57,82 +71,9 @@ public class Cita {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JPanel panel = new JPanel();
-		panel.setBounds(461, 67, 420, 250);
-		frame.getContentPane().add(panel);
-		panel.setLayout(null);
-
-
-		int x=0;
-		int pos=0;		
-		for(int i=0;i<5;i++) {
-			
-			int y=0;
-			for(int j=0;j<7;j++) {
-
-				JPanel panel_1 = new JPanel();
-				panel_1.setBounds(y, x, 60, 50);
-				panel.add(panel_1);
-				panel_1.setLayout(null);
-
-				JLabel lblNewLabel = new JLabel("New label");
-				lblNewLabel.setText(String.valueOf(pos));			
-				lblNewLabel.setBounds(10, 11, 40, 28);
-				panel_1.add(lblNewLabel);
-				pos++;
-				y+=60;
-
-			}
-
-			x+=50;
-		}
-
-		JLabel mes_Anio = new JLabel("");
-		mes_Anio.setBounds(518, 22, 302, 30);
-		frame.getContentPane().add(mes_Anio);
-
-		JButton boton_MesAtras = new JButton("");
-		boton_MesAtras.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		boton_MesAtras.setBounds(461, 22, 59, 30);
-		frame.getContentPane().add(boton_MesAtras);
-
-		JButton boton_Mesdelante = new JButton("");
-		boton_Mesdelante.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		boton_Mesdelante.setBounds(821, 22, 59, 30);
-		frame.getContentPane().add(boton_Mesdelante);
-
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(461, 365, 420, 130);
-		frame.getContentPane().add(scrollPane);
-
-		tabla_Horarios = new JTable();
-		tabla_Horarios.setModel(new DefaultTableModel(
-				new Object[][] {
-					{new String("8:00")},
-					{new String("9:00")},
-					{new String("10:00")},
-					{new String("11:00")},
-					{new String("12:00")},
-					{new String("13:00")},
-					{new String("16:00")},
-					{new String("17:00")},
-					{new String("18:00")},
-					{new String("19:00")},
-				},
-				new String[] {
-						""
-				}
-				));
-		tabla_Horarios.getColumnModel().getColumn(0).setPreferredWidth(386);
-		scrollPane.setViewportView(tabla_Horarios);
+		JCalendar calendario=new JCalendar();
+		calendario.setBounds(461, 67, 420, 250);
+		frame.getContentPane().add(calendario);
 		
 		JLabel lblInformacinMascota = new JLabel("Informaci\u00F3n Mascota");
 		lblInformacinMascota.setBounds(24, 26, 384, 26);
@@ -141,5 +82,44 @@ public class Cita {
 		JButton btnGuardarCita = new JButton("Guardar Cita");
 		btnGuardarCita.setBounds(611, 545, 121, 23);
 		frame.getContentPane().add(btnGuardarCita);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBounds(461, 366, 420, 132);
+		frame.getContentPane().add(scrollPane);
+		
+		JPanel panel__Horas = new JPanel();
+		scrollPane.setViewportView(panel__Horas);
+		
+		panel__Horas.setPreferredSize(new Dimension(418,horarios.length*31));
+		panel__Horas.setLayout(null);
+		
+		int pos_Lab=0;
+		boolean hora_Libre=true;
+		for (int i = 0; i < horarios.length; i++) {
+			JPanel panel_1 = new JPanel();
+			panel_1.setBounds(0, pos_Lab, 418, 31);
+			panel__Horas.add(panel_1);
+			panel_1.setLayout(null);
+			
+			JLabel lblNewLabel_1 = new JLabel(horarios[i]);
+			lblNewLabel_1.setBounds(27, 11, 46, 14);
+			panel_1.add(lblNewLabel_1);
+			
+			//control
+			if(horarios[i]=="13:00") {
+				hora_Libre=false;
+			}
+			
+			if(hora_Libre==true) {
+				panel_1.setBackground(Color.GREEN);
+			}
+			else {
+				panel_1.setBackground(Color.RED);
+			}
+			hora_Libre=true;
+			pos_Lab+=31;
+		}
 	}
 }
